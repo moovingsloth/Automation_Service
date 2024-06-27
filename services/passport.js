c8f91c53f17c49b49b6eab1fd510e76e9861315f
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-06-27 12:37:54 +0900

    google login

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.use(new GoogleStrategy(
     {
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: 'https://shielded-wildwood-99077-257122bdbfd6.herokuapp.com/auth/google/callback',
+    callbackURL: '/auth/google/callback',
+    proxy: true
     }, 
     (accessToken, refreshToken, profile, done) => {
 
