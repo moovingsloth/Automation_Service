bbfe740825440f27a77f0fb73d7b8e70ec312556
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-07-24 22:45:35 +0900

    testing email sending:

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,3 @@
-const { mailgunDomail } = require("./dev");
-
 // prod.js - production keys here!!
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys');
 
@@ -19,6 +20,7 @@ module.exoprts = app => {
         });
 
         //Great place to send an email!
-        const mailer = new Mailer();
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        mailer.send();
     });
 };
