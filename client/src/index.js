2bbad261355622158ef883ddda4fa247f6489334
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-07-24 23:27:01 +0900

    axios added

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import { thunk as reduxThunk } from 'redux-thunk';
 
 import App from './components/App'
 import reducers   from './reducers';
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers , {}, applyMiddleware(reduxThunk));   
 
