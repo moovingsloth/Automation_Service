a0c8eacbfadaae9c784a142e4dda7f125295881c
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-08-01 21:31:55 +0900

    recipients complete, login, credits fix neede

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
 const Mailgun = require('mailgun.js');
 const formData = require('form-data');
 const keys = require('../config/keys');
@@ -12,37 +10,31 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-    const { title, subject, body, recipients } = req.body;
-
-    // 설문 조사 객체 생성
-    const survey = new Survey({
-      title,
-      subject,
-      body,
-      recipients,
-      dateSent: Date.now(),
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
     });
 
-    const mailer = new Mailer(survey, surveyTemplate(survey));
+    app.post('/api/surveys', async (req, res) => {
+        const { title, subject, body, recipients } = req.body;
+
+        // 설문 조사 객체 생성
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })), // 이메일 주소를 객체로 변환
+            dateSent: Date.now(),
+        });
+
+        const mailer = new Mailer(survey, surveyTemplate(survey));
 
     try {
       // 이메일 전송
-      const msg = await mg.messages.create(keys.mailGunDomain, mailer);
-      console.log(msg); // 응답 데이터 로깅
-
-      // 설문 조사 저장
-      await survey.save();
-
-      // 크레딧 차감
-      req.user.credits -= 1;
-      const user = await req.user.save();
-
-      // 클라이언트에게 응답 전송
-      res.status(200).send(user);
+      mg.messages.create(keys.mailGunDomain, mailer);
     } catch (err) {
       console.error(err); // 오류 로깅
       res.status(500).send({ error: err.message }); // 클라이언트에게 오류 응답 전송
     }
   });
 };
+
--- services/Mailer.js
+++ services/Mailer.js
@@ -1,11 +1,16 @@
 class Mailer {
     constructor({ subject, recipients }, content) {
         this.from = "Lee Dongwon <dlehddnjs245tommy@gmail.com>";
-        this.to ="dlehddnjs245@naver.com";
+        this.to = this.formatRecipients(recipients); // recipients를 형식화
         this.subject = subject;
         this.html = content;
         this.text = "Testing some Mailgun awesomeness!";
     }
-};
+
+    formatRecipients(recipients) {
+        // recipients가 배열 형태로 들어올 것이라고 가정
+        return recipients.map(({ email }) => email).join(', ');
+    }
+}
 
 module.exports = Mailer;
