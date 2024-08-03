91d6956641650ae8e7812b733e836fa17456e386
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-08-03 23:47:56 +0900

    history -> navigage

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,9 +13,9 @@ export const handleToken = token => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = (values, history) => async dispatch => {
+export const submitSurvey = (values, navigate) => async dispatch => {
   const res = await axios.post('/api/surveys', values);
 
-  history.push('/surveys');
+  navigate('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
